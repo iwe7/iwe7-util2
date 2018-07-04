b45aa6aec48dfb6ba00e87fd224c058a23a77c47
iwe7 <admin@meepo.com.cn>
2018-07-04 16:41:01 +0800

    添加设置模块接口

--- src/lib/iwe7-util2.service.ts
+++ src/lib/iwe7-util2.service.ts
@@ -1,31 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, isDevMode } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Iwe7Util2Service {
-
+  m: string = 'iwe7_worker';
   constructor(
     private http: HttpClient
   ) { }
+
+  setM(m: string) {
+    this.m = m;
+  }
 
   wurl(m: string, mdo: string) {
     let url = '.';
     if (isDevMode()) {
       url = 'http://test.meepo.com.cn/web';
     }
-    return `${url}/index.php?c=site&a=entry&do=api&m=iwe7_worker&model=${m}&mdo=${mdo}`;
+    return `${url}/index.php?c=site&a=entry&do=api&m=${this.m}&model=${m}&mdo=${mdo}`;
   }
 
-  wget(m: string, mdo: string, params: any) {
+  wget<T>(m: string, mdo: string, params: any): Observable<T> {
     const url = this.wurl(m, mdo);
-    return this.http.get(url, { withCredentials: true, params: params });
+    return this.http.get<T>(url, { withCredentials: true, params: params });
   }
 
-  wpost(m: string, mdo: string, body: any, params: any = {}) {
+  wpost<T>(m: string, mdo: string, body: any, params: any = {}): Observable<T> {
     const url = this.wurl(m, mdo);
-    return this.http.post(url, body, { withCredentials: true, params: params });
+    return this.http.post<T>(url, body, { withCredentials: true, params: params });
   }
 
 }
